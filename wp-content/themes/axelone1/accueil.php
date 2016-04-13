ceef35da7dbfd739ee85774e8476282436f48b9e
ManuLdb <emmanuellorenzoldb@gmail.com>
2016-04-13 20:44:54 +0200

    ajouter de l'avant derniere div

--- wp-content/themes/axelone1/accueil.php
+++ wp-content/themes/axelone1/accueil.php
@@ -7,32 +7,51 @@
 <main>
     <div class="col-md-12" id="first">
     </div>
-    <div class="col-md-6 firstriangle">
-        <div class="bordtriangle1"></div>
-    </div>
-    <div class="col-md-6 firstriangle">
-        <div class="bordtriangle2"></div>
-    </div>
-    <div class="col-md-6 firstriangle">
-        <div class="triangle1"></div>
-    </div>
-    <div class="col-md-6 firstriangle">
-        <div class="triangle2"></div>
+        <div class="col-md-6 firstriangle">
+            <div class="bordtriangle1"></div>
+        </div>
+        <div class="col-md-6 firstriangle">
+            <div class="bordtriangle2"></div>
+        </div>
+        <div class="col-md-6 firstriangle">
+            <div class="triangle1"></div>
+        </div>
+        <div class="col-md-6 firstriangle">
+            <div class="triangle2"></div>
     </div>
     <div class="col-md-12" id="deuz">
         <div class="col-md-12">
-            <div class="col-md-6 firstriangle">
-                <div class="bordtriangle3"></div>
-            </div>
-            <div class="col-md-6 firstriangle">
-                <div class="bordtriangle4"></div>
-            </div>
+
             <div class="col-md-6 firstriangle">
                 <div class="triangle3"></div>
             </div>
             <div class="col-md-6 firstriangle">
                 <div class="triangle4"></div>
+
             </div>
         </div>
     </div>
+    <div class="col-md-12" id="troiz">
+    </div>
+    <div class="col-md-12" id="katre">
+    </div>
+    <div class="col-md-12" id="cink">
+
+        <div class="col-md-6 firstriangle">
+            <div class="bordtriangle3"></div>
+        </div>
+        <div class="col-md-6 firstriangle">
+            <div class="bordtriangle4"></div>
+        </div>
+        <div class="col-md-6 firstriangle">
+            <div class="triangle5"></div>
+        </div>
+        <div class="col-md-6 firstriangle">
+            <div class="triangle6"></div>
+        </div>
+        <div class="col-md-6 triangle7"></div>
+        <div class="col-md-6 triangle8"></div>
+
+    </div>
+    <div class="col-md-6 " id="siss"></div>
 </main>
