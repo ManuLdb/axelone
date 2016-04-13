ced9aa03424deee7065e075ad685ca65e7f777d5
ManuLdb <emmanuellorenzoldb@gmail.com>
2016-04-13 18:08:31 +0200

    on va y arriver

--- wp-content/themes/axelone1/accueil.php
+++ wp-content/themes/axelone1/accueil.php
@@ -7,7 +7,32 @@
 <main>
     <div class="col-md-12" id="first">
     </div>
+    <div class="col-md-6 firstriangle">
+        <div class="bordtriangle1"></div>
+    </div>
+    <div class="col-md-6 firstriangle">
+        <div class="bordtriangle2"></div>
+    </div>
+    <div class="col-md-6 firstriangle">
+        <div class="triangle1"></div>
+    </div>
+    <div class="col-md-6 firstriangle">
+        <div class="triangle2"></div>
+    </div>
     <div class="col-md-12" id="deuz">
-
+        <div class="col-md-12">
+            <div class="col-md-6 firstriangle">
+                <div class="bordtriangle3"></div>
+            </div>
+            <div class="col-md-6 firstriangle">
+                <div class="bordtriangle4"></div>
+            </div>
+            <div class="col-md-6 firstriangle">
+                <div class="triangle3"></div>
+            </div>
+            <div class="col-md-6 firstriangle">
+                <div class="triangle4"></div>
+            </div>
+        </div>
     </div>
 </main>
