6a2c4ba57aed6d83299fdf0e2d71a62ae040ea95
Fabien <fmarie.houeix@gmail.com>
2016-04-13 16:55:30 +0200

    Modif txt à propos

--- wp-content/themes/axelone1/propos.php
+++ wp-content/themes/axelone1/propos.php
@@ -69,8 +69,7 @@ Template Name: à propos
             <h2>L'ENTREPRISE</h2>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc est velit, ullamcorper blandit auctor a,
-                iaculis sit amet erat. Suspendisse pulvinar s purus, id gravida purus. Quisque ullamcorper imperdiet
-                lorem, vel facilisis augue ornare dignissim. Donec diam diam, suscipit vitae quam sit amet, venenatis
+                iaculis sit amet erat. Suspendisse pulvinar s purus, id gravida purus. Venenatis
                 vehicula nisi. Quisque interdum tortor vitae lectus efficitur, eget egestas purus finibus. Cum sociis
                 natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
 
