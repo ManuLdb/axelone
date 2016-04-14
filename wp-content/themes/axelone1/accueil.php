cc628e420c367e647f45ffcec47ef66d56609750
ManuLdb <emmanuellorenzoldb@gmail.com>
2016-04-14 16:11:09 +0200

    ajout partenaire et contact sans le contact

--- wp-content/themes/axelone1/accueil.php
+++ wp-content/themes/axelone1/accueil.php
@@ -66,6 +66,21 @@
     <div class="col-md-12" id="troiz">
     </div>
     <div class="col-md-12" id="katre">
+        <h3>NOS PARTENAIRES</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum debitis facere illo labore libero obcaecati
+            quae repellendus veritatis? Corporis doloremque exercitationem illum, inventore maiores
+            molestias nostrum numquam saepe! Architecto, quod?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum debitis facere illo labore libero obcaecati
+            quae repellendus veritatis? Corporis doloremque exercitationem illum, inventore maiores
+            molestias nostrum numquam saepe! Architecto, quod?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum debitis facere illo labore libero obcaecati
+            quae repellendus veritatis? Corporis doloremque exercitationem illum, inventore maiores
+            molestias nostrum numquam saepe! Architecto, quod?
+        </p>
+        <div class="blockjaune">
+
+        </div>
     </div>
     <div class="col-md-12" id="cink">
 
@@ -85,5 +100,16 @@
         <div class="col-md-6 triangle8"></div>
 
     </div>
-    <div class="col-md-6 " id="siss"></div>
+    <div class="col-md-6 " id="siss">
+        <div class="col-md-6">
+            <p>
+                Nunc vel sem a arcu mattis luctus et quis quam. Donec faucibus pulvinar velit, eget finibus risus ornare tristique.
+                Duis elit diam, hendrerit non dictum quis, egestas in nulla. Pellentesque consectetur at dolor ac dapibus. Phasellus feugiat libero id ex scelerisque,
+                congue laoreet sapien finibus. Sed et neque eu tortor suscipit venenatis. Praesent felis tortor, sodales quis commodo sit amet, rutrum vitae quam.
+            </p>
+        </div>
+        <div class="col-md-6">
+
+        </div>
+    </div>
 </main>
