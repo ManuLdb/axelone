48effd63c5fb0a8b81b5193e22b1674dcfe4d204
Theov <theo.voison@gmail.com>
2016-04-15 19:12:26 +0200

    modif glyphicons -> fortawesome

--- wp-content/themes/axelone1/accueil.php
+++ wp-content/themes/axelone1/accueil.php
@@ -26,7 +26,7 @@
         <div class="col-md-12">
             <div class="col-md-4 deuztext">
                 <h2 id="titre">SERVICE 1</h2>
-                <i class="glyph glyphicon glyphicon-adjust" style="font-size:40px;"></i>
+                <i class="fa fa-lightbulb-o glyph" style="font-size:40px;"></i>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
@@ -36,7 +36,7 @@
             </div>
             <div class="col-md-4 deuztext">
                 <h2 id="titre">SERVICE 2</h2>
-                <i class="glyph glyphicon glyphicon-adjust" style="font-size:40px;"></i>
+                <i class="fa fa-thumbs-o-up glyph" aria-hidden="true" style="font-size:40px;"></i>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
@@ -46,7 +46,7 @@
             </div>
             <div class="col-md-4 deuztext">
                 <h2 id="titre">SERVICE 3</h2>
-                <i class="glyph glyphicon glyphicon-adjust" style="font-size:40px;"></i>
+                <i class="fa fa-camera-retro glyph" style="font-size:40px;"></i>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
