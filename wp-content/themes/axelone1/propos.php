608ac83011906e58e09d68b6c8e6d33aca682368
Theov <theo.voison@gmail.com>
2016-04-20 16:02:17 +0200

    Revert "Revert "Merge remote-tracking branch 'refs/remotes/origin/master'""
    This reverts commit a9ae020df0619f6e2d16022ead33f87c74c6fc70.

--- wp-content/themes/axelone1/propos.php
+++ wp-content/themes/axelone1/propos.php
@@ -5,59 +5,15 @@ Template Name: à propos
 ?>
 
 <?php get_header(); ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>AXELONE</title>
-
-    <!-- Bootstrap -->
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
-          integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
-            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-            crossorigin="anonymous"></script>
-
-    <!-- CSS -->
-
-    <link href="ccs/style.css" type="text/css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
-
-
-</head>
-<body>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-
-            <div class="titre">
-                <a href="index.html"> <img src=""> </a>
-
-
+    
+    <div class="col-md-12 triangleapropos">
+            <div class="col-md-6 firstriangle">
+                <div class="triangle3apropos"></div>
+            </div>
+            <div class="col-md-6 firstriangle">
+                <div class="triangle4apropos"></div>
             </div>
         </div>
-
-    </div>
-
-    <div class="titre">
-
-        <h1>A PROPOS - L'ENTREPRISE</h1>
-
-    </div>
 
     <div class="row">
         <div class="col-md-6">
@@ -66,8 +22,7 @@ Template Name: à propos
 
         </div>
 
-        <div class="col-md-4">
-
+        <div class="col-md-4 entreprise">
             <h2>L'ENTREPRISE</h2>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc est velit, ullamcorper blandit auctor a,
@@ -89,8 +44,7 @@ Template Name: à propos
     </div>
 
     <div class="row">
-
-        <div class="col-md-4">
+        <div class="col-md-4 histoire">
 
             <h2>L'HISTOIRE</h2>
 
@@ -111,56 +65,4 @@ Template Name: à propos
 
         </div>
 
-        <div classe="img2" class="col-md-6">
-
-            <img src="img/puma.jpg" class="img-responsive" alt=""/>
-
-        </div>
-
-    </div>
-
-
-
-        <div class="col-md-4"><h2>SERVICE 1</h2><i class="glyph glyphicon glyphicon-adjust" style="font-size:40px;"></i><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequa.</p></div>
-    <div class="col-md-4"><h2>SERVICE 2</h2><i class="glyph glyphicon glyphicon-adjust" style="font-size:40px;"></i><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequa.</p></div>
-
-    <div class="col-md-4 deuztext">
-        <h2>SERVICE 3</h2>
-        <i class="glyph glyphicon glyphicon-adjust" style="font-size:40px;"></i>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequa.
-        </p>
-    </div>
-
-   
-       <div class="row">
-          
-          <div class="col-md-12"> <img src="" class="img-responsive" id="industrie" alt=""/></div>
-        
-       </div>
-
-    
-
-
-
-
-
-
-
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-
-
-</body>
-</html>
-
+       
