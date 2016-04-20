bdf0922c72f276998a96474be007f7c53274bc7e
ManuLdb <emmanuellorenzoldb@gmail.com>
2016-04-20 15:29:05 +0200

    ajout formation

---
+++ wp-content/themes/axelone1/formation.php
@@ -0,0 +1,28 @@
+<?php
+// Template Name: Formation
+?>
+
+<?php get_header(); ?>
+<main>
+    <div id="formation1">
+        <h2>FORMATION</h2>
+        <div class="col-md-12">
+            <div class="col-md-6 firstriangle">
+                <div class="formationtriangle1"></div>
+            </div>
+            <div class="col-md-6 firstriangle">
+                <div class="formationtriangle2"></div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="col-md-6 firstriangle">
+                <div class="formationbordtriangle1"></div>
+            </div>
+            <div class="col-md-6 firstriangle">
+                <div class="formationbordtriangle2"></div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
+
