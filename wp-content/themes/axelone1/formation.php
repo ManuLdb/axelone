6b5891e486a245ad721e2b553fa367d5332b503b
Fabien <fmarie.houeix@gmail.com>
2016-04-20 17:05:58 +0200

    revert ?

--- wp-content/themes/axelone1/formation.php
+++ wp-content/themes/axelone1/formation.php
@@ -14,44 +14,15 @@
                 <div class="formationtriangle2"></div>
             </div>
         </div>
-    </div>
-    <div id="formation2">
-        <iframe class="formationvideo" src="https://www.youtube.com/embed/dsDTJ__jioo" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div id="formation3">
-       <h3>NOS COURS</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores consectetur cumque delectus
-            explicabo fuga illo illum in maiores nisi numquam pariatur perferendis quam ratione reiciendis sequi sunt temporibus, ullam.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores consectetur cumque delectus
-            explicabo fuga illo illum in maiores nisi numquam pariatur perferendis quam ratione reiciendis sequi sunt temporibus, ullam.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores consectetur cumque delectus
-            explicabo fuga illo illum in maiores nisi numquam pariatur perferendis quam ratione reiciendis sequi sunt temporibus, ullam.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores consectetur cumque delectus
-            explicabo fuga illo illum in maiores nisi numquam pariatur perferendis quam ratione reiciendis sequi sunt temporibus, ullam.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores consectetur cumque delectus
-            explicabo fuga illo illum in maiores nisi numquam pariatur perferendis quam ratione reiciendis sequi sunt temporibus, ullam.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores consectetur cumque delectus
-            explicabo fuga illo illum in maiores nisi numquam pariatur perferendis quam ratione reiciendis sequi sunt temporibus, ullam.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores consectetur cumque delectus
-            explicabo fuga illo illum in maiores nisi numquam pariatur perferendis quam ratione reiciendis sequi sunt temporibus, ullam.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores consectetur cumque delectus
-            explicabo fuga illo illum in maiores nisi numquam pariatur perferendis quam ratione reiciendis sequi sunt temporibus, ullam.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores consectetur cumque delectus
-            explicabo fuga illo illum in maiores nisi numquam pariatur perferendis quam ratione reiciendis sequi sunt temporibus, ullam.
-        </p>
-    </div>
-    <div id="formation4">
-        <div class="col-md-6 firstriangle">
-            <div class="formationtriangle3"></div>
-        </div>
-        <div class="col-md-6 firstriangle">
-            <div class="formationtriangle4"></div>
-            <div class="col-md-6">
-                <img src="../wp-content/themes/axelone1/img/madame.png" alt="madame">
+        <div class="col-md-12">
+            <div class="col-md-6 firstriangle">
+                <div class="formationbordtriangle1"></div>
             </div>
-            <div class="col-md-6">
-
+            <div class="col-md-6 firstriangle">
+                <div class="formationbordtriangle2"></div>
             </div>
         </div>
     </div>
 </main>
+<?php get_footer(); ?>
+
