a1e34396ba1384ec75682d15ef3d36cdc5f279c3
Fabien <fmarie.houeix@gmail.com>
2016-06-01 15:15:33 +0200

    Mise à jour contenu

--- wp-content/themes/axelone1/propos.php
+++ wp-content/themes/axelone1/propos.php
@@ -32,17 +32,25 @@ Template Name: à propos
            
            <div class="texteentreprise">
                <div class="col-md-6">
-                   <p><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</b></p>
+                   <p><b>J’ai créée axel one en 2009 suite à une observation réfléchie du marché du conseil et de la formation : l'axe 1 est l’origine du nom de sa société axel one. Chacune des expériences permet de prendre du recul sur les priorités à mettre en œuvre dans l'entreprise pour se focaliser sur les bonnes actions.</b></p>
                    
-                    <p> Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                    <p> La théorie des contraintes, présentée par Eliyahu M. GOLDRATT dans son best-seller "LE BUT", est le support de tout. La rentabilité des projets, des affaires et des moindres actions réalisées par tous les partenaires internes ou externes devient une source de valeur ajoutée pour réaliser le seul but de l'entreprise : gagner de l'argent.
+                    Les formations proposées découlent de ces propos de E. M. Goldratt :
+                    Elles sont destinées à tous les interlocuteurs motivés pour optimiser le quotidien de leur activité et de celle de leurs collaborateurs;
+                    Elles génèrent un langage commun et international autour des flux de matières et d'informations. </p>
                </div>
            </div>
     
            <div class="texteentreprise">
                <div class="col-md-6">
-                   <p><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</b></p>
+                   <p><b>Le professionnalisme des collaborateurs, le sérieux du suivi de la théorie des contraintes,  l'accompagnement au changement  et l'amélioration continue permettent des résultats immédiats et durables.</b></p>
                    
-                    <p> Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                    <p> Axel one a pour vocation le déploiement de formations certifiantes :
+                         Basics of Supply Chain Management délivrée par apics
+                         Certified in Production and Inventory Management délivrée par apics
+                         The Fresh Connection – jeu de rôle simulant les contraintes commerciales et industrielles. Vulgarisation de la formation et apprentissage d’un double lexique anglais-français
+                        Mise en pratique avec le contexte et les contraintes des participants
+                        Conseil personnalisé au TPE / PME / grandes entreprises. </p>
                </div>
            </div>
             </div>
