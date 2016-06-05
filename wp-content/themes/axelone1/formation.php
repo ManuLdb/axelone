f7fa2341385e5e6e218b0c003854cbe1d4fc955f
ManuLdb <emmanuellorenzoldb@gmail.com>
2016-06-05 18:53:43 +0200

    ajout contenu/video formation'

--- wp-content/themes/axelone1/formation.php
+++ wp-content/themes/axelone1/formation.php
@@ -29,21 +29,25 @@
         </div>
     </div>
     <div id="formation2">
-        <iframe class="formationvideo" src="https://www.youtube.com/embed/dsDTJ__jioo" frameborder="0" allowfullscreen></iframe>
+        <iframe class="formationvideo" src="https://www.youtube.com/embed/eb55fW9V35c" frameborder="0" allowfullscreen></iframe>
     </div>
     <div id="formation3">
         <div class="col-md-6">
             <h2>NOS CONSEILS</h2>
-             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                 Alias corporis culpa est fugiat harum laborum minus mollitia sapiente tenetur ullam. Assumenda consequuntur facere laudantium magni quo repudiandae,
-                 temporibus voluptas voluptates?
+             <p>
+                 Validation et exploitation de données sous excel : vous avez un fichier excel très utile pour l'entreprise, avec nombreux utilisateurs, et vous souhaitez le sécuriser, l'améliorer; je suis force de proposition et de mise en oeuvre. L'idée est aussi d'intégrer ces fichiers pour harmoniser les moyens informatiques et de faire d'Excel un maquettage pour utiliser les outils informatiques adéquats.
+
+                 Assistance à maitrise d'ouvrage : Le lien entre les équipes informatiques et les équipes opérationnelles. Forte d'une formation polytechnique, je pourrais converser avec les équipes techniques de l'informatique et les équipes techniques du terrain. Faciliter les échanges et optimiser la production des outils informatiques pour une meilleure qualité de service
+
              </p>
         </div>
         <div class="col-md-6">
             <h2>NOS COURS</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Alias corporis culpa est fugiat harum laborum minus mollitia sapiente tenetur ullam. Assumenda consequuntur facere laudantium magni quo repudiandae,
-                temporibus voluptas voluptates?
+            <p>
+                Sensibilisation à la qualité : il s'agit par un jeu de rôle de se retrouver dans une situation de producteur d'avions en papiers, de comprendre les difficultés de répondre à un cahier des charges au meilleur coût et d'arriver à la notion de qualité : qualité perçue, coût d'obtention de la qualité, coût de la non-qualité
+                5S : (extrait de wikipedia)
+                La méthode des 5 « S » (en anglais the 5 S's) est une technique de management japonaise visant à l'amélioration continue des tâches effectuées dans les entreprises. Élaborée dans le cadre du système de production de Toyota (en anglais Toyota Production System ou TPS), elle tire son appellation de la première lettre de chacune de cinq opérations constituant autant de mots d'ordre ou principes simples.
+
             </p>
         </div>
 
